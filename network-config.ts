e5bd925a9810dd4d9a78d02c266218f9593ed2af
Dwight Lyle <dwightjl@gmail.com>
2023-09-30 13:38:44 -0600

    Add networks and update packages

--- network-config.ts
+++ network-config.ts
@@ -18,6 +18,22 @@ export const networkConfig: NetworkConfig = {
       '0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15',
     fundAmount: '15000000000000000000', // 15 LINK
   },
+  '43113': {
+    name: 'avalancheFuji',
+    linkToken: '0x0b9d5D9136855f6FEc3c0993feE6E9CE8a297846',
+    vrfCoordinatorV2: '0x2eD832Ba664535e5886b75D64C46EB9a228C2610',
+    keyHash:
+      '0x354d2f95da55398f44b7cff77da56283d9c6c829a4bdf1bbcaf2ad6a4d081f61',
+    fundAmount: '5000000000000000000', // 5 LINK
+  },
+  '80001': {
+    name: 'polygonMumbai',
+    linkToken: '0x326C977E6efc84E512bB9C30f76E30c160eD06FB',
+    vrfCoordinatorV2: '0x7a1BaC17Ccc5b313516C5E16fb24f7659aA5ebed',
+    keyHash:
+      '0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f',
+    fundAmount: '5000000000000000000', // 5 LINK
+  },
   '31337': {
     name: 'hardhat',
     linkToken: '0x326C977E6efc84E512bB9C30f76E30c160eD06FB',
